90ba46b07d5c191d38a0be4eaed72adfb80a4211
chiruvolurevathi <80575439+chiruvolurevathi@users.noreply.github.com>
2022-06-25 16:30:30 +0530

    Update updateData.py

--- updateData.py
+++ updateData.py
@@ -2,20 +2,18 @@ from tkinter import *
 import tkinter.messagebox as MessageBox
 import mysql.connector as mysql
 
-try:
-    mydb = mysql.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="revathi"
+mydb = mysql.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="revathi"
 
-    )
-    mycursor = mydb.cursor()
+)
+mycursor = mydb.cursor()
 
-    mycursor.execute("UPDATE studentdetails SET aadharCard= 222222 WHERE Id=1;")
-    mycursor.execute("commit")
-    MessageBox.showinfo("Status","Data updated SuccessFully")
-except:
-    MessageBox.showinfo("Status", "Data is not valid Please Enter valid Data")
+mycursor.execute("UPDATE studentdetails SET aadharCard= 222222 WHERE Id=1;")
+mycursor.execute("commit")
+MessageBox.showinfo("Status","Data updated SuccessFully")
+
 
 mydb.close()
