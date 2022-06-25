0a09fe9c6a9bddc476ba32087aa62a08a4c30ea3
chiruvolurevathi <80575439+chiruvolurevathi@users.noreply.github.com>
2022-06-25 20:52:05 +0530

    Update insertData.py

--- insertData.py
+++ insertData.py
@@ -12,12 +12,9 @@ try:
 
     )
     mycursor = mydb.cursor()
-    mycursor.execute(
-        "INSERT INTO studentdetails(Id,name,aadharCard,parentName,phoneNumber,class,section,feesStatus)VALUES (1,'Adithya',1252421,'pranav',923434322,7,'A','paid');")
-
+    mycursor.execute("INSERT INTO studentdetails(Id,name,aadharCard,parentName,phoneNumber,class,section,feesStatus)VALUES (1,'Adithya',1252421,'pranav',923434322,7,'A','paid');")
     mycursor.execute("commit")
     MessageBox.showinfo("Status", "Data Inserted SuccessFully")
-
     mydb.close()
 except:
     MessageBox.showinfo("Status", "Duplicate values are not allowed")
